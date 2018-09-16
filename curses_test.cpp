1b081bd70cd33a466d4e9d626490959175ec637b
tanacchi <q111026d@mail.kyutech.jp>
2018-09-17 00:23:51 +0900

    Output inputed char from getch function for test

--- curses_test.cpp
+++ curses_test.cpp
@@ -6,13 +6,15 @@ int main()
 {
   initscr();
   keypad(stdscr, TRUE);
-
+  
   int count{0};
   while (true) {
-    if (getch()) {
+    char input;
+    if (input = getch()) {
       ++count;
     }
     mvprintw(20, 15, "Hello, ncurses. [%d]\n", count);
+    mvprintw(21, 15, "Input char: [%c]\n", (char)input);
     refresh();
   }
   endwin();

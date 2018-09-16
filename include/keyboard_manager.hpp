65780ab2b931abc73d2c4eed7a476623f1efdb0b
tanacchi <q111026d@mail.kyutech.jp>
2018-09-17 02:32:20 +0900

    Add KeyState enum on KeyboardManager class

--- include/keyboard_manager.hpp
+++ include/keyboard_manager.hpp
@@ -5,6 +5,16 @@
 
 class KeyboardManager
 {
+public:
+  enum KeyState
+  {
+    Space   = 32,
+    Up      = 3,
+    Dowm    = 2,
+    Right   = 5,
+    Left    = 4,
+    Unknown =-1
+  };
 };
 
 #endif  // INCLUDED_KEYBOARD_MANAGER_HPP

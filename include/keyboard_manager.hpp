bc149c98ca0adc9268ddc6b447bd3225371cd1c0
tanacchi <q111026d@mail.kyutech.jp>
2018-09-17 01:21:11 +0900

    Add KeyboardManager class

---
+++ include/keyboard_manager.hpp
@@ -0,0 +1,10 @@
+#ifndef INCLUDED_KEYBOARD_MANAGER_HPP
+#define INCLUDED_KEYBOARD_MANAGER_HPP
+
+#include <ncurses.h>
+
+class KeyboardManager
+{
+};
+
+#endif  // INCLUDED_KEYBOARD_MANAGER_HPP
---
+++ src/keyboard_manager.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../include/keyboard_manager.hpp"
+
+int main()
+{
+  KeyboardManager km{};
+  std::cout << "Hello, rouge_game." << std::endl;
+  return 0;
+}

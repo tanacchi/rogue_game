6c88b1bbc5e87f050b11403504725ecfe73e87a8
tanacchi <q111026d@mail.kyutech.jp>
2018-09-18 00:41:25 +0900

    Add file reader on map_to_json_simple.cpp

--- map/map_to_json_simple.cpp
+++ map/map_to_json_simple.cpp
@@ -6,18 +6,29 @@
 #include <boost/optional.hpp>
 #include <stdexcept>
 #include <vector>
+#include <fstream>
 
 int main(int argc, char** argv)
 {
+  std::string text_map_data{};
   try {
     std::vector<std::string> arg_list{argv, argv + argc};
     std::string text_map_filname{arg_list[1]};
     std::cout << text_map_filname << std::endl;
+    std::ifstream read_file{};
+    read_file.open(text_map_filname, std::ios::in);
+    std::string input_buff{};
+    while (!read_file.eof()) {
+      std::getline(read_file, input_buff);
+      std::cout << '$' << input_buff << '$' << std::endl;
+      text_map_data += input_buff;
+    }
   }
   catch (const std::logic_error& e) {
     std::cout << "[Usage]: argv[1]: text_map_file, argv[2]: width, argv[3]: height" << std::endl;
   }
-
+  std::cout << "Map data:" << text_map_data << std::endl;
+  return 0;
   boost::property_tree::ptree map_data;
   map_data.put("Map.width", 30);
   map_data.put("Map.height", 10);

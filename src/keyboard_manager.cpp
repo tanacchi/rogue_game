2a8a8b4521a7deaf1d17a8d8f83095f577bf978d
tanacchi <q111026d@mail.kyutech.jp>
2018-09-17 15:36:47 +0900

    Modify get_key method with switch statement

--- src/keyboard_manager.cpp
+++ src/keyboard_manager.cpp
@@ -20,7 +20,17 @@ KeyboardManager::KeyState KeyboardManager::get_key()
   if (input != static_cast<int>(KeyboardManager::KeyState::Unknown)) count++;
   mvprintw(20, 15, "Count: [%d]\n", count);
   mvprintw(21, 15, "Input char: [%c]\n", (char)input);
-  return static_cast<KeyState>(input);  // FIX: Unknown のフィルターが機能していない
+  // return static_cast<KeyState>(input);  // FIX: Unknown のフィルターが機能していない
+  switch (static_cast<KeyState>(input)) {                //  Refactor Required
+  case KeyState::Space: return KeyState::Space;
+  case KeyState::Enter: return KeyState::Enter;
+  case KeyState::Back:  return KeyState::Back;
+  case KeyState::Up:    return KeyState::Up;
+  case KeyState::Down:  return KeyState::Down;
+  case KeyState::Right: return KeyState::Right;
+  case KeyState::Left:  return KeyState::Left;
+  default:              return KeyState::Unknown;
+  }
 }
 
 std::string key_to_str(KeyboardManager::KeyState key)

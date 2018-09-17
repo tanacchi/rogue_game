47c74f613dffb1661316804010db6efc2436b1e5
tanacchi <q111026d@mail.kyutech.jp>
2018-09-17 16:05:48 +0900

    Add main sourse file

--- src/keyboard_manager.cpp
+++ src/keyboard_manager.cpp
@@ -32,38 +32,3 @@ KeyboardManager::KeyState KeyboardManager::get_key()
   default:              return KeyState::Unknown;
   }
 }
-
-std::string key_to_str(KeyboardManager::KeyState key)
-{
-  switch (key) {
-  case KeyboardManager::KeyState::Space:
-    return "Space";
-  case KeyboardManager::KeyState::Enter:
-    return "Enter";
-  case KeyboardManager::KeyState::Back:
-    return "Back";
-  case KeyboardManager::KeyState::Up:
-    return "Up";
-  case KeyboardManager::KeyState::Down:
-    return "Down";
-  case KeyboardManager::KeyState::Right:
-    return "Right";
-  case KeyboardManager::KeyState::Left:
-    return "Left";
-  case KeyboardManager::KeyState::Unknown:
-    return "Unknown";
-  default:
-    return "???????";
-  }
-}
-
-int main()
-{
-  KeyboardManager km{};
-  std::cout << "Hello, rouge_game." << std::endl;
-  while (true) {
-    KeyboardManager::KeyState key{km.get_key()};
-    mvprintw(22, 15, "Input KeyState: [%s]\n", key_to_str(key).data());
-  }
-  return 0;
-}

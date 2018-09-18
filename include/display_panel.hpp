b679e27f99a44205804b1d004ca2287c8e8d1a06
tanacchi <q111026d@mail.kyutech.jp>
2018-09-19 01:52:58 +0900

    Remove unnecessary method DisplayPanel::draw_outline

--- include/display_panel.hpp
+++ include/display_panel.hpp
@@ -8,15 +8,12 @@ class DisplayPanel
 public:
   DisplayPanel(std::size_t x, std::size_t y, std::size_t width, std::size_t height);
   ~DisplayPanel();
-  void draw_outline() const noexcept;
 protected:
   WINDOW *win_;
 private:
   const std::size_t x_;
   const std::size_t y_;
   const std::size_t height_;
-  const std::string row_boarder_;
-  const std::string col_boarder_;
 };
 
 #endif  // INCLUDED_DISPLAY_PANEL_HPP
--- src/display_panel.cpp
+++ src/display_panel.cpp
@@ -8,22 +8,11 @@ DisplayPanel::DisplayPanel(std::size_t x, std::size_t y,
   : win_{newwin(height, width, y, x)},
     x_{x},
     y_{y},
-    height_{height},
-    row_boarder_(width, '-'),
-    col_boarder_{std::string("|") + std::string(width-2, ' ') + std::string("|")}
+    height_{height}
 {
 }
 
 DisplayPanel::~DisplayPanel()
 {
   delwin(win_);
-}
-
-void DisplayPanel::draw_outline() const noexcept
-{
-  mvprintw(y_, x_, row_boarder_.data());
-  for (std::size_t i{1}; i < height_-1; ++i) {
-    mvprintw(y_+i, x_, col_boarder_.data());
-  }
-  mvprintw(y_+height_-1, x_, row_boarder_.data());
 }

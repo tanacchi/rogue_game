ce0ae25161362cf283b5b353fecc20914ca37180
tanacchi <q111026d@mail.kyutech.jp>
2018-09-19 01:57:10 +0900

    Remove unecessary test code on KeyboardManager

--- src/keyboard_manager.cpp
+++ src/keyboard_manager.cpp
@@ -2,10 +2,5 @@
 
 KeyboardManager::KeyState KeyboardManager::get_key() const noexcept
 {
-  static int count{0};
-  int input{getch()};
-  if (input != static_cast<int>(KeyboardManager::KeyState::Unknown)) count++;
-  mvprintw(20, 15, "Count: [%d]\n", count);
-  mvprintw(21, 15, "Input char: [%c]\n", (char)input);
-  return static_cast<KeyState>(input);  // FIX: Unknown のフィルターが機能していない
+  return static_cast<KeyState>(getch());
 }

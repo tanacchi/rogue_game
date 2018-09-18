9cfdb02fa76e6d9369a1bcd37ff1b9e6bde425cb
tanacchi <q111026d@mail.kyutech.jp>
2018-09-19 01:21:58 +0900

    Show map text on map display window

--- src/map_display.cpp
+++ src/map_display.cpp
@@ -13,9 +13,10 @@ namespace map {
   {
     for (int y{0}; y < map.height; ++y) {
       for (int x{0}; x < map.width; ++x) {
-        mvprintw(y, x, "%c", map.elems[y*map.width+x].symbol);
+        mvwprintw(win_, y, x, "%c", map.elems[y*map.width+x].symbol);
       }
-      mvprintw(y, map.width, "\n");
+      mvwprintw(win_, y, map.width, "\n");
     }
+    wrefresh(win_);
   }
 }

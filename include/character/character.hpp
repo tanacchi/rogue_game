ce5a2939a21920fd010c499cf409ca33d098d69c
tanacchi <q111026d@mail.kyutech.jp>
2018-09-22 18:25:21 +0900

    Add Character's x, y getter

--- include/character/character.hpp
+++ include/character/character.hpp
@@ -7,6 +7,8 @@ class Character
 {
 public:
   Character(std::size_t x, std::size_t y);
+  std::size_t get_x();
+  std::size_t get_y();
 private:
   std::size_t x_;
   std::size_t y_;
--- src/rogue_game/character/character.cpp
+++ src/rogue_game/character/character.cpp
@@ -4,3 +4,14 @@ Character::Character(std::size_t x, std::size_t y)
   : x_{x}, y_{y}
 {
 }
+
+std::size_t Character::get_x()
+{
+  return x_;
+}
+
+std::size_t Character::get_y()
+{
+  return y_;
+}
+

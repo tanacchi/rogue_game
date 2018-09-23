0b7e2cbf064fcb9d6c647bc47f3771e4271e7157
tanacchi <q111026d@mail.kyutech.jp>
2018-09-23 21:20:18 +0900

    Add Floor class

---
+++ include/map/floor.hpp
@@ -0,0 +1,18 @@
+#ifndef INCLUDED_FLOOR_HPP
+#define INCLUDED_FLOOR_HPP
+
+#include <map/map_elem.hpp>
+
+namespace map
+{
+  class Floor : public MapElem
+  {
+  public:
+    Floor(std::string type)
+      : MapElem(type)
+    {
+    }
+  };
+}
+
+#endif  // INCLUDED_FLOOR_HPP

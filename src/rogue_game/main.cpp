2daa9f37f0b27987617b12bb3d9e047dd676fc69
tanacchi <q111026d@mail.kyutech.jp>
2018-09-29 16:09:52 +0900

    Remove unnecessary function on main

--- src/rogue_game/main.cpp
+++ src/rogue_game/main.cpp
@@ -1,27 +1,5 @@
 #include <rogue_game.hpp>
 
-std::string key_to_str(KeyboardManager::KeyState key)
-{
-  switch (key) {
-  case KeyboardManager::KeyState::Space:
-    return "Space";
-  case KeyboardManager::KeyState::Enter:
-    return "Enter";
-  case KeyboardManager::KeyState::Back:
-    return "Back";
-  case KeyboardManager::KeyState::Up:
-    return "Up";
-  case KeyboardManager::KeyState::Down:
-    return "Down";
-  case KeyboardManager::KeyState::Right:
-    return "Right";
-  case KeyboardManager::KeyState::Left:
-    return "Left";
-  default:
-    return "Unknown";
-  }
-}
-
 int main()
 {
   initscr();

95c188ac721e87e9b3dc0f6c94f8e70cea6dc37f
tanacchi <q111026d@mail.kyutech.jp>
2018-10-01 01:56:31 +0900

    Add simple class to visualize player's status

---
+++ include/display/player_display.hpp
@@ -0,0 +1,14 @@
+#ifndef INCLUDED_PLAYER_DISPLAY_HPP
+#define INCLUDED_PLAYER_DISPLAY_HPP
+
+#include <display/display_panel.hpp>
+#include <character/player.hpp>
+
+class PlayerDisplay : public DisplayPanel
+{
+public:
+  PlayerDisplay(std::size_t x, std::size_t y, std::size_t width, std::size_t height);
+  void show(const character::Player& player);
+};
+
+#endif  // INCLUDED_PLAYER_DISPLAY_HPP
--- src/rogue_game/main.cpp
+++ src/rogue_game/main.cpp
@@ -8,10 +8,12 @@ int main()
   
   map::Map map = map::read_map(map_dir + "json/tmp_sample_map.json");
   map::MapDisplay md{5, 4, map.width, map.height};
+  PlayerDisplay   pd{70, 30, 20, 10};
   std::unique_ptr<KeyboardManager> km_ptr{new KeyboardManager};
   character::Player player{map::Point{10, 15}, std::move(km_ptr)};
   while (true) {
     md.show(map, player);
+    pd.show(player);
     refresh();
     player.update(map);
   }

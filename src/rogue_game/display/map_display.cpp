dc4ebe43518b2adda44634985ce9962c7d9d6e82
tanacchi <q111026d@mail.kyutech.jp>
2018-10-03 02:16:34 +0900

    Use std::unique_ptr for WINDOW pointer (ncurses)

--- src/rogue_game/display/map_display.cpp
+++ src/rogue_game/display/map_display.cpp
@@ -11,15 +11,15 @@ namespace map {
   {
     for (std::size_t y{0}; y < map.height; ++y) {
       for (std::size_t x{0}; x < map.width; ++x) {
-        mvwprintw(win_, y, x, "%c", map.dungeon_layer[y*map.width+x]->symbol);
+        mvwprintw(win_.get(), y, x, "%c", map.dungeon_layer[y*map.width+x]->symbol);
       }
-      mvwprintw(win_, y, map.width, "\n");
+      mvwprintw(win_.get(), y, map.width, "\n");
     }
     for (auto item : map.item_layer) {
       Point pos{item->get_position()};
-      mvwprintw(win_, pos.get_y(), pos.get_x(), "%c", item->symbol);
+      mvwprintw(win_.get(), pos.get_y(), pos.get_x(), "%c", item->symbol);
     }
-    mvwprintw(win_, character.get_position().get_y(), character.get_position().get_x(), "@");
-    wrefresh(win_);
+    mvwprintw(win_.get(), character.get_position().get_y(), character.get_position().get_x(), "@");
+    wrefresh(win_.get());
   }
 }

bcfeb0191c5c12ca2869f4461afe71f810f4397f
tanacchi <q111026d@mail.kyutech.jp>
2018-10-04 12:06:48 +0900

    Add default move constructor on MapElem class

--- include/map/map_elem.hpp
+++ include/map/map_elem.hpp
@@ -8,6 +8,7 @@ namespace map
   class MapElem {
   public:
     MapElem(std::string type);
+    MapElem(MapElem&&) noexcept = default;
     virtual ~MapElem() = default;
     const std::string type;
     const char symbol;

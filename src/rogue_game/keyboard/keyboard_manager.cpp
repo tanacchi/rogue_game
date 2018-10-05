47eb420d396a755b4269ff4fd53d1f6dd9b05605
tanacchi <q111026d@mail.kyutech.jp>
2018-10-05 15:38:49 +0900

    Correct indent of key_state_map initializer

--- src/rogue_game/keyboard/keyboard_manager.cpp
+++ src/rogue_game/keyboard/keyboard_manager.cpp
@@ -8,11 +8,11 @@ const KeyboardManager::KeyState KeyboardManager::get_key() noexcept
   static const std::map<int, KeyboardManager::KeyState> key_state_table = {
     {32,            KeyState::Space},
     {10,            KeyState::Enter},
-    {KEY_BACKSPACE, KeyState::Back},
-    {KEY_UP,        KeyState::Up},
-    {KEY_DOWN,      KeyState::Down},
+    {KEY_BACKSPACE, KeyState::Back },
+    {KEY_UP,        KeyState::Up   },
+    {KEY_DOWN,      KeyState::Down },
     {KEY_RIGHT,     KeyState::Right},
-    {KEY_LEFT,      KeyState::Left}
+    {KEY_LEFT,      KeyState::Left }
   };
   const std::map<int, KeyboardManager::KeyState>::const_iterator key_state_itr{key_state_table.find(getch())};
   return (key_state_itr != key_state_table.end()) ? key_state_itr->second : KeyState::Unknown;

c35fa15e3aba8b4dd4531bd68ef14309b9ba62ad
tanacchi <q111026d@mail.kyutech.jp>
2018-10-05 22:12:18 +0900

    Pass KeyState variable tp Player::update method

--- src/rogue_game/game_master.cpp
+++ src/rogue_game/game_master.cpp
@@ -17,12 +17,13 @@ void GameMaster::update()
   map::Map map = map::read_map(map_dir + "json/tmp_sample_map.json");
   map::MapDisplay md{5, 4, map.width, map.height};
   PlayerDisplay   pd{70, 30, 20, 10};
-  // std::unique_ptr<KeyboardManager> km_ptr{new KeyboardManager};
+  KeyboardManager keyboard{};
   character::Player player{map::Point{10, 15}};
   while (true) {
     md.show(map, player);
     pd.show(player);
     refresh();
-    // player.update(map);
+    const KeyboardManager::KeyState key_state{keyboard.get_key()};
+    player.update(map, key_state);
   }
 }

1b9c26f420f34db40697660e15eaddf27c5ba6f3
tanacchi <q111026d@mail.kyutech.jp>
2018-10-13 15:26:12 +0900

    Add TextMap constructor from map_strings

--- include/map/text_map.hpp
+++ include/map/text_map.hpp
@@ -8,15 +8,20 @@ namespace map
   namespace generator
   {
     struct TextMap {
+    public:
+      TextMap(const std::vector<std::string>& map_strings);
       std::size_t width{};
       std::size_t height{};
       std::string text{};
 
       void show() const;
+    private:
+      const std::size_t get_width(const std::vector<std::string>& map_strings);
+      const std::size_t get_height(const std::vector<std::string>& map_strings);
+      const std::string get_text(const std::vector<std::string>& map_strings);
     };
 
-    std::vector<std::string> read_map_strings(std::string filename);
-    TextMap get_text_map_obj(std::vector<std::string> map_strings);
+    const std::vector<std::string> read_map_strings(const std::string& filename);
   }
 }
 
--- src/map_maker/main.cpp
+++ src/map_maker/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
     const std::vector<std::string> arg_list{argv, argv + argc};
     const std::string text_map_filname{arg_list[1]};
     const std::vector<std::string> map_strings{map::generator::read_map_strings(text_map_filname)};
-    map::generator::TextMap text_map{map::generator::get_text_map_obj(map_strings)};
+    map::generator::TextMap text_map{map_strings};
     text_map.show();
     map::generator::MapGenerator map_generator{text_map, "ver_0.1.0.0_map"};
     map_generator.set_configs();

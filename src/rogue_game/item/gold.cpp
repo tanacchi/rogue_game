6c86dec95c049c690a43e2ed266ed53656b5b0e6
tanacchi <q111026d@mail.kyutech.jp>
2018-10-21 02:05:47 +0900

    Add Gold::use method

--- src/rogue_game/item/gold.cpp
+++ src/rogue_game/item/gold.cpp
@@ -1,4 +1,5 @@
 #include <item/gold.hpp>
+#include <character/player.hpp>
 
 namespace item
 {
@@ -6,6 +7,11 @@ namespace item
     : Item("gold"),
       amount_{amount}
   {
+  }
+
+  void Gold::use(character::Player* const player_ptr)
+  {
+    player_ptr->add_money(amount_);
   }
 
   std::ostream& operator<<(std::ostream& os, const Gold& gold)

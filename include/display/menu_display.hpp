14fe1f86f36fb5f843743002a58092d5628a5901
tanacchi <q111026d@mail.kyutech.jp>
2018-10-21 17:08:23 +0900

    Add menu_strings setter for MenuDisplay class

--- include/display/menu_display.hpp
+++ include/display/menu_display.hpp
@@ -1,6 +1,9 @@
 #ifndef INCLUDED_MUNU_DISPLAY_HPP
 #define INCLUDED_MUNU_DISPLAY_HPP
 
+#include <vector>
+#include <string>
+
 #include <display/display_panel.hpp>
 
 namespace menu
@@ -10,8 +13,10 @@ namespace menu
   public:
     MenuDisplay(std::size_t x, std::size_t y,
                 std::size_t width = 10ul, std::size_t height = 10ul);
+    void set_menu(std::vector<std::string>&& menu);
   private:
-    int selected_index;
+    std::vector<std::string> menu_strings_{};
+    int selected_index_;
   };
 }
 
--- src/rogue_game/display/menu_display.cpp
+++ src/rogue_game/display/menu_display.cpp
@@ -4,7 +4,12 @@ namespace menu
 {
   MenuDisplay::MenuDisplay(std::size_t x, std::size_t y, std::size_t width, std::size_t height)
     : DisplayPanel(x, y, width, height),
-      selected_index{0}
+      selected_index_{0}
   {
+  }
+
+  void MenuDisplay::set_menu(std::vector<std::string>&& menu)
+  {
+    menu_strings_ = menu;
   }
 }

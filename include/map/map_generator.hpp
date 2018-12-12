c6f2b9758575e804e27bf5adcb151704e1ad2ab3
tanacchi <q111026d@mail.kyutech.jp>
2018-12-12 11:10:59 +0900

    Remove map::generator namespace

--- include/map/map_generator.hpp
+++ include/map/map_generator.hpp
@@ -13,30 +13,27 @@
 
 namespace map
 {
-  namespace generator
+  // マップデータ（json）を生成するクラス
+  // ループで回すことになるからファンクタ化しようと思うけど後回し
+  class MapGenerator
   {
-    // マップデータ（json）を生成するクラス
-    // ループで回すことになるからファンクタ化しようと思うけど後回し
-    class MapGenerator
-    {
-    public:
-      MapGenerator(const TextMap& text_map, std::string output_filename = "tmp_sample_map");
+  public:
+    MapGenerator(const TextMap& text_map, std::string output_filename = "tmp_sample_map");
 
-      // ダンジョン要素・アイテム要素の ptree の配列を TextMap より生成する
-      void set_configs();
+    // ダンジョン要素・アイテム要素の ptree の配列を TextMap より生成する
+    void set_configs();
 
-      // ptree の配列から json ファイルを書き出す
-      void write_json_map() const;
-    private:
-      const TextMap text_map_;
-      const std::string output_filename_;
+    // ptree の配列から json ファイルを書き出す
+    void write_json_map() const;
+  private:
+    const TextMap text_map_;
+    const std::string output_filename_;
 
-      // ダンジョン要素の ptree の配列
-      std::vector<boost::property_tree::ptree> dungeon_configs_;
-      // アイテム要素の ptree の配列
-      std::vector<boost::property_tree::ptree> item_configs_;
-    };
-  }
+    // ダンジョン要素の ptree の配列
+    std::vector<boost::property_tree::ptree> dungeon_configs_;
+    // アイテム要素の ptree の配列
+    std::vector<boost::property_tree::ptree> item_configs_;
+  };
 }
 
 #endif  // INCLUDED_MAP_GENERATOR_HPP

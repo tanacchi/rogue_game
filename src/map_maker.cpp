9a15ea9a842efce1b981c6a1d3576824aa8bd0c9
tanacchi <q111026d@mail.kyutech.jp>
2018-12-13 18:30:15 +0900

    Fix typo

--- src/map_maker.cpp
+++ src/map_maker.cpp
@@ -6,8 +6,8 @@ int main(int argc, char** argv)
 {
   try {
     const std::vector<std::string> arg_list{argv, argv + argc};
-    const std::string text_map_filname{arg_list[1]};
-    const std::vector<std::string> map_strings{map::read_map_strings(text_map_filname)};
+    const std::string text_map_filename{arg_list[1]};
+    const std::vector<std::string> map_strings{map::read_map_strings(text_map_filename)};
     map::TextMap text_map{map_strings};
     std::cout << text_map << std::endl;
     LOG_VALUES(text_map);

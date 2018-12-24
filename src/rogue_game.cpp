faf7118c94c64eba2879a75af6615893ef311410
tanacchi <q111026d@mail.kyutech.jp>
2018-12-24 20:09:18 +0900

    Add test code for dungeon-series inserters

--- src/rogue_game.cpp
+++ src/rogue_game.cpp
@@ -3,12 +3,37 @@
 
 #include <character/character.hpp>
 
+// Log test
+
+#include <dungeon/door.hpp>
+#include <dungeon/floor.hpp>
+#include <dungeon/horizontal_wall.hpp>
+#include <dungeon/none.hpp>
+#include <dungeon/path.hpp>
+#include <dungeon/vertical_wall.hpp>
+
+//
+
 int main()
 {
   initscr();
   keypad(stdscr, TRUE);
   curs_set(0);
   refresh();
+
+
+  // Log test
+
+  dungeon::Door door{};
+  dungeon::Floor floor{};
+  dungeon::HorizontalWall horizontal_wall{};
+  dungeon::None none{};
+  dungeon::Path path{};
+  dungeon::VerticalWall vertical_wall{};
+
+  LOG_VALUES(door, floor, horizontal_wall, none, path, vertical_wall);
+
+  //
 
   GameMaster master{};
   while (true) {

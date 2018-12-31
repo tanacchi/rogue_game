5f71e9cd6dc62785bc0f66c63e8622be738a34fe
tanacchi <q111026d@mail.kyutech.jp>
2018-12-31 16:51:17 +0900

    Do not ignore rogue_game.hpp header

---
+++ include/rogue_game/rogue_game.hpp
@@ -0,0 +1,12 @@
+#ifndef INCLUDED_ROGUE_GAME_HPP
+#define INCLUDED_ROGUE_GAME_HPP
+
+#include <keyboard/keyboard_manager.hpp>
+#include <display/map_display.hpp>
+#include <display/player_display.hpp>
+#include <debug/logger.hpp>
+#include <boost/filesystem.hpp>
+
+const std::string map_dir{boost::filesystem::current_path().string() + "/map/"};
+
+#endif  // INCLUDED_ROGUE_GAME_HPP

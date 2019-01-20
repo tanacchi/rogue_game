35f86958419dc8dc850e3c993ee4cccd24ec5cbd
tanacchi <q111026d@mail.kyutech.jp>
2019-01-20 15:58:13 +0900

    Show player direction with PlayerDisplay::show

--- src/rogue_game/display/player_display.cpp
+++ src/rogue_game/display/player_display.cpp
@@ -15,10 +15,14 @@ void PlayerDisplay::show(const character::Player& player)
   // 言語が違うか
   // 位置情報は logger に任せるつもり
   map::Point<int> pos{player.get_position()};
+  map::Point<int> dir{player.get_direction()};
   mvwprintw(win_.get(), 0, 0, "Position: ");
   mvwprintw(win_.get(), 1, 2, "x: %d", pos.get_x());
   mvwprintw(win_.get(), 2, 2, "y: %d", pos.get_y());
-  mvwprintw(win_.get(), 3, 0, "money: %d", player.get_money());
+  mvwprintw(win_.get(), 3, 0, "Direction: ");
+  mvwprintw(win_.get(), 4, 2, "dx: %d", dir.get_x());
+  mvwprintw(win_.get(), 5, 2, "yy: %d", dir.get_y());
+  mvwprintw(win_.get(), 6, 0, "money: %d", player.get_money());
 
   wrefresh(win_.get());
 }

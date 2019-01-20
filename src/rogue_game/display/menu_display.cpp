f8516f97c0c9badc37a052e485d7c40297613f04
tanacchi <q111026d@mail.kyutech.jp>
2019-01-20 12:57:12 +0900

    Refactor MenuDisplay::show function

--- src/rogue_game/display/menu_display.cpp
+++ src/rogue_game/display/menu_display.cpp
@@ -5,7 +5,7 @@ namespace menu
 {
   MenuDisplay::MenuDisplay(std::size_t x, std::size_t y, std::size_t width, std::size_t height)
     : DisplayPanel(x, y, width, height),
-      selected_index_{0}
+      selected_index_{}
   {
   }
 
@@ -17,14 +17,14 @@ namespace menu
   void MenuDisplay::show() const
   {
     wclear(win_.get());
-    box(win_.get(), 0, 0);
     for (std::size_t i{0}, size{menu_strings_.size()}; i < size; ++i) {
       if (i == selected_index_) {
         wattron(win_.get(), A_REVERSE);
       }
-      mvwprintw(win_.get(), 1 + i, 1, " %d : %s ", i, menu_strings_[i].c_str());
+      mvwinsstr(win_.get(), 1 + i, 1, menu_strings_[i].c_str());
       wattrset(win_.get(), A_NORMAL);
     }
+    box(win_.get(), ACS_VLINE, ACS_HLINE);
     wrefresh(win_.get());
   }
 

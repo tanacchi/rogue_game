9e405a91eb2c9f6e0b4cdedcd3063a6b97798672
tanacchi <q111026d@mail.kyutech.jp>
2019-01-31 20:56:18 +0900

    Change definition of KeyType values

--- include/keyboard/key_manager.hpp
+++ include/keyboard/key_manager.hpp
@@ -10,16 +10,16 @@ class KeyManager
   public:
     using KeyType = std::uint8_t;
 
-    static const KeyType Space;
-    static const KeyType Enter;
-    static const KeyType Back;
-    static const KeyType Up;
-    static const KeyType Down;
-    static const KeyType Right;
-    static const KeyType Left;
-    static const KeyType Null;
-    static const KeyType Enable;
-    static const KeyType Arrow;
+    static const KeyType Space  {0b00000001};
+    static const KeyType Enter  {0b00000010};
+    static const KeyType Back   {0b00000100};
+    static const KeyType Up     {0b00001000};
+    static const KeyType Down   {0b00010000};
+    static const KeyType Right  {0b00100000};
+    static const KeyType Left   {0b01000000};
+    static const KeyType Null   {0b10000000};
+    static const KeyType Enable {0b01111111};
+    static const KeyType Arrow  {0b01111000};
 
     static const std::unordered_map<int, KeyManager::KeyType> key_state_table;
 
--- src/rogue_game/keyboard/key_manager.cpp
+++ src/rogue_game/keyboard/key_manager.cpp
@@ -1,15 +1,15 @@
 #include <keyboard/key_manager.hpp>
 
-const KeyManager::KeyType KeyManager::Space  = 0x01 << 0;
-const KeyManager::KeyType KeyManager::Enter  = 0x01 << 1;
-const KeyManager::KeyType KeyManager::Back   = 0x01 << 2;
-const KeyManager::KeyType KeyManager::Up     = 0x01 << 3;
-const KeyManager::KeyType KeyManager::Down   = 0x01 << 4;
-const KeyManager::KeyType KeyManager::Right  = 0x01 << 5;
-const KeyManager::KeyType KeyManager::Left   = 0x01 << 6;
-const KeyManager::KeyType KeyManager::Null   = 0x01 << 7;
-const KeyManager::KeyType KeyManager::Enable = 0xff >> 1;
-const KeyManager::KeyType KeyManager::Arrow  = KeyManager::Up | KeyManager::Down | KeyManager::Right | KeyManager::Left;
+const KeyManager::KeyType KeyManager::Space;
+const KeyManager::KeyType KeyManager::Enter;
+const KeyManager::KeyType KeyManager::Back;
+const KeyManager::KeyType KeyManager::Up;
+const KeyManager::KeyType KeyManager::Down;
+const KeyManager::KeyType KeyManager::Right;
+const KeyManager::KeyType KeyManager::Left;
+const KeyManager::KeyType KeyManager::Null;
+const KeyManager::KeyType KeyManager::Enable;
+const KeyManager::KeyType KeyManager::Arrow;
 
 const std::unordered_map<int, KeyManager::KeyType> KeyManager::key_state_table{{
   {32,            KeyManager::Space},

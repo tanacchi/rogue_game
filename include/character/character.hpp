32db5afbcf71be1a980f4ca771a22362eb97039d
tanacchi <q111026d@mail.kyutech.jp>
2019-04-23 14:20:06 +0900

    Remove character namespace from header

--- include/character/character.hpp
+++ include/character/character.hpp
@@ -6,16 +6,14 @@
 #include <debug/logger.hpp>
 #include <map/point.hpp>
 
-namespace character
+// キャラクターの基底クラス
+// Player に加え Enemy が継承する予定
+class Character
 {
-  // キャラクターの基底クラス
-  // Player に加え Enemy が継承する予定
-  class Character
-  {
   public:
     // デフォルト引数はそのうちなくす予定
     Character(map::Point<int> position = map::zero);
-    
+
     // キャラクターの位置を取得
     map::Point<int> get_position() const;  // const& にする？
 
@@ -25,7 +23,6 @@ namespace character
     friend std::ostream& operator<<(std::ostream& os, const Character& character);
   protected:
     map::Point<int> position_;
-  };
-}
+};
 
 #endif  // INCLUDED_CHARACTER_HPP

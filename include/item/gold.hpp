ccde19d8b60ed8e26b59402abff7c81cc25c4fa0
tanacchi <q111026d@mail.kyutech.jp>
2019-04-23 14:23:52 +0900

    Remove item namespace from header

--- include/item/gold.hpp
+++ include/item/gold.hpp
@@ -5,18 +5,15 @@
 
 #include <item/item.hpp>
 
-namespace item
+// 触れると所持金が増えるアイテム
+class Gold : public Item
 {
-  // 触れると所持金が増えるアイテム
-  class Gold : public Item
-  {
   public:
     Gold(std::size_t amount = 100);
     void use(character::Player* const player_ptr);
     std::string to_string() const;
   private:
     const std::size_t amount_;
-  };
-}
+};
 
 #endif  // INCLUDED_GOLD_HPP
--- include/item/item.hpp
+++ include/item/item.hpp
@@ -8,18 +8,13 @@
 #include <map/map_elem.hpp>
 #include <map/point.hpp>
 
-namespace character
-{
-  class Player;
-}
+class Player;
 
-namespace item
+// アイテムの基底クラス
+// マップデータに記述されるので MapElem を継承
+// 取得（インベントリに追加） → 使用のプロセスを設ける予定
+class Item : public ::map::MapElem
 {
-  // アイテムの基底クラス
-  // マップデータに記述されるので MapElem を継承
-  // 取得（インベントリに追加） → 使用のプロセスを設ける予定
-  class Item : public ::map::MapElem
-  {
   public:
     Item(std::string type);
     Item(const Item& item) = default;
@@ -27,9 +22,8 @@ namespace item
     virtual void use(character::Player* const player_ptr);
     virtual ~Item() = default;
     virtual std::string to_string() const;
-  };
+};
 
-  using ItemPtr = std::shared_ptr<Item>;
-}
+using ItemPtr = std::shared_ptr<Item>;
 
 #endif  // INCLUDED_ITEM_HPP

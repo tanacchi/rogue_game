afdd79ac6f9263b46e31571ceaf499d6888e4fa7
tanacchi <q111026d@mail.kyutech.jp>
2019-04-29 22:04:33 +0900

    Add Menu toggler on MenuHandler class

--- src/rogue_game/menu/menu_handler.cpp
+++ src/rogue_game/menu/menu_handler.cpp
@@ -3,13 +3,29 @@
 
 GameStatus MenuHandler::operator()(GameMaster& master)
 {
+  Menu        menu{};
   MenuDisplay menu_display{100, 10, 30, 16};
-  Menu menu;
+  KeyManager  keyboard{};
 
-  menu_display.show(menu, selected_index_);
-
-  getch();
-
+  while (true)
+  {
+    menu_display.show(menu, selected_index_);
+    keyboard.update();
+    switch (keyboard.get())
+    {
+      case KeyManager::Up:
+        if (selected_index_ > 0)
+          --selected_index_;
+        break;
+      case KeyManager::Down:
+        ++selected_index_;
+        break;
+      case KeyManager::Space:
+      case KeyManager::Back:
+        menu_display.hide();
+        return GameStatus{Mode::Dungeon, Task::Show};
+    }
+  }
   menu_display.hide();
 
   return GameStatus{};

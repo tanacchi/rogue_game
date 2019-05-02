b651ed04d078236c0b7083db98585eaf2c443732
tanacchi <q111026d@mail.kyutech.jp>
2019-05-03 03:23:47 +0900

    Add HP toggler and getter on Charater class

--- src/rogue_game/character/character.cpp
+++ src/rogue_game/character/character.cpp
@@ -22,3 +22,22 @@ std::ostream& operator<<(std::ostream& os, const Character& character)
   os << " { position : " << character.position_ << " }";
   return os;
 }
+
+std::size_t Character::get_hit_point() const noexcept
+{
+  return hit_point_;
+}
+
+std::size_t Character::get_max_hit_point() const noexcept
+{
+  return max_hit_point_;
+}
+
+void Character::toggle_hit_point(std::size_t amount)
+{
+  hit_point_ += amount;
+  if (hit_point_ > max_hit_point_)
+    hit_point_ = max_hit_point_;
+  else if (hit_point_ < 0)
+    hit_point_ = 0;
+}

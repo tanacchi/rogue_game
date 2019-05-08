ebcb00c672acf5f52d16d4692109d1ad537c664e
tanacchi <q111026d@mail.kyutech.jp>
2019-05-09 00:07:02 +0900

    Add Inventory class

---
+++ include/character/inventory.hpp
@@ -0,0 +1,24 @@
+#ifndef INCLUDED_INVENTORY_HPP
+#define INCLUDED_INVENTORY_HPP
+
+#include <map>
+#include <vector>
+
+#include <item/item.hpp>
+
+class Inventory
+{
+  public:
+    using ContentType = std::multimap<std::string, const ItemPtr>;
+    using ElemType = ContentType::value_type;
+
+    Inventory();
+    void store(const ItemPtr& item_ptr);
+    void dispose(std::size_t target_index);
+    std::vector<std::string> get_item_names() const noexcept;
+
+  private:
+    ContentType content_;
+};
+
+#endif  // INCLUDED_INVENTORY_HPP

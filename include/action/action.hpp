6fc64f3ba062e7de470d4baf8ac2335849225d18
tanacchi <q111026d@mail.kyutech.jp>
2019-07-16 17:04:32 +0900

    Make Action's member functions virtual

--- include/action/action.hpp
+++ include/action/action.hpp
@@ -3,14 +3,13 @@
 
 #include <memory>
 
-#include <game_master/game_status.hpp>
-
 template <typename T>
 class UsageTag {};
 class ConsumeTag : UsageTag<ConsumeTag> {};
 class PutTag : UsageTag<PutTag> {};
 
 class GameMaster;
+class GameStatus;
 
 template <typename T, typename U>
 class Action
@@ -18,11 +17,11 @@ class Action
   public:
     using Target = T;
     using Usage  = U;
-    GameStatus do_action(const std::shared_ptr<GameMaster>& master);
+    virtual GameStatus do_action(const std::shared_ptr<GameMaster>& master);
 
   private:
-    GameStatus do_it(const std::shared_ptr<GameMaster>& master, ConsumeTag);
-    GameStatus do_it(const std::shared_ptr<GameMaster>& master, PutTag);
+    virtual GameStatus do_it(const std::shared_ptr<GameMaster>& master, ConsumeTag) = 0;
+    virtual GameStatus do_it(const std::shared_ptr<GameMaster>& master, PutTag) = 0;
 };
 
 #endif  // INCLUDED_ACTION_HPP
--- src/rogue_game/action/action.cpp
+++ src/rogue_game/action/action.cpp
@@ -1,20 +1,9 @@
 #include <action/action.hpp>
 #include <game_master/game_master.hpp>
+#include <game_master/game_status.hpp>
 
 template <typename T, typename U>
 GameStatus Action<T, U>::do_action(const std::shared_ptr<GameMaster>& master)
 {
   return do_it(master, Usage{});
-}
-
-template <typename T, typename U>
-GameStatus Action<T, U>::do_it(const std::shared_ptr<GameMaster>& master, ConsumeTag)
-{
-  return GameStatus{};
-}
-
-template <typename T, typename U>
-GameStatus Action<T, U>::do_it(const std::shared_ptr<GameMaster>& master, PutTag)
-{
-  return GameStatus{};
 }

db53fdba2e0be2100def9d7a77930dc3d02a25f6
tanacchi <q111026d@mail.kyutech.jp>
2019-09-08 19:12:47 +0900

    Refactor MapDisplay::show function

--- src/rogue_game/display/map_display.cpp
+++ src/rogue_game/display/map_display.cpp
@@ -17,29 +17,16 @@ void MapDisplay::show(const Map& map, const Player& player)
   {
     for (std::size_t x{0}; x < map.width; ++x)
     {
-      mvwaddch(win_.get(), y, x, map.dungeon_layer[y][x]->symbol);
+      auto visible_symbol{map.dungeon_layer[y][x]->symbol};
+      const auto& stepped_item_itr{map.item_layer.find(Point<int>{x, y})};
+      if (stepped_item_itr != map.item_layer.end())
+        visible_symbol = stepped_item_itr->second->symbol;
+      if (map.hidden_layer[y][x])
+        visible_symbol = ' ';
+      mvwaddch(win_.get(), y, x, visible_symbol);
     }
-    mvwaddch(win_.get(), y, map.width, '\n');
-  }
-  // Display items
-  for (const auto& item : map.item_layer)
-  {
-    mvwaddch(win_.get(), item.first.get_y(), item.first.get_x(), item.second->symbol | A_BOLD);
   }
 
-  // Hide unknown area
-  for (std::size_t y{0}; y < map.height; ++y)
-  {
-    for (std::size_t x{0}; x < map.width; ++x)
-    {
-      if (map.hidden_layer[y][x])
-      {
-        mvwaddch(win_.get(), y, x, ' ');
-      }
-    }
-    mvwaddch(win_.get(), y, map.width, '\n');
-  }
-  // Display player position and direction
   mvwaddch(win_.get(), player.get_position().get_y(), player.get_position().get_x(), '@' | A_BOLD);
   auto sight{player.get_position() + player.get_direction()};
   mvwchgat(win_.get(), sight.get_y(), sight.get_x(), 1, A_BOLD | A_UNDERLINE, 0, nullptr);
